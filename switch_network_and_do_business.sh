73d6cba6b88e9cd37c96b91e0c1155b7e97df02b
Turtlebot3 <you@example.com>
2022-11-21 19:27:01 +0000

    Initial commit of files

---
+++ switch_network_and_do_business.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+echo "Switching networks"
+sudo rm /etc/netplan/50-cloud-init.yaml
+sudo cp /etc/netplan_AGITJ/50-cloud-init.yaml /etc/netplan/50-cloud-init.yaml
+sudo netplan apply
+echo "Changed network and waiting"
+sleep 10
+######################################
+####PUT DESIRED COMMMANDS TO RUN WHILE ON INTERNET ACCESS HERE
+## for apt or apt-get, use apt -y install to automatically answer Yes to install question
+wget https://github.com/ROBOTIS-GIT/OpenCR-Binaries/raw/master/turtlebot3/ROS2/latest/opencr_update.tar.bz2
+#sudo apt-get -y install libc6:armhf
+#sudo apt-get update
+#######################################
+sudo rm /etc/netplan/50-cloud-init.yaml
+sudo cp /etc/netplan_ROS/50-cloud-init.yaml /etc/netplan/50-cloud-init.yaml
+sudo netplan apply
+echo -en "\007"
