c968b5f2cb49fc492f3133ab4838980cfb97ab50
kgallowa <kgallowa@usna.edu>
2022-11-21 21:25:52 +0000

    Instead of saving the files under directories in /etc, we now copy the files from the folders included in the repo

--- switch_network_and_do_business.sh
+++ switch_network_and_do_business.sh
@@ -2,18 +2,18 @@
 
 echo "Switching networks"
 sudo rm /etc/netplan/50-cloud-init.yaml
-sudo cp /etc/netplan_AGITJ/50-cloud-init.yaml /etc/netplan/50-cloud-init.yaml
+sudo cp ./netplan_internet/50-cloud-init.yaml /etc/netplan/50-cloud-init.yaml
 sudo netplan apply
 echo "Changed network and waiting"
-sleep 10
+sleep 20
 ######################################
 ####PUT DESIRED COMMMANDS TO RUN WHILE ON INTERNET ACCESS HERE
 ## for apt or apt-get, use apt -y install to automatically answer Yes to install question
-wget https://github.com/ROBOTIS-GIT/OpenCR-Binaries/raw/master/turtlebot3/ROS2/latest/opencr_update.tar.bz2
+#wget https://github.com/ROBOTIS-GIT/OpenCR-Binaries/raw/master/turtlebot3/ROS2/latest/opencr_update.tar.bz2
 #sudo apt-get -y install libc6:armhf
-#sudo apt-get update
+sudo apt-get update
 #######################################
 sudo rm /etc/netplan/50-cloud-init.yaml
-sudo cp /etc/netplan_ROS/50-cloud-init.yaml /etc/netplan/50-cloud-init.yaml
+sudo cp ./netplan_local/50-cloud-init.yaml /etc/netplan/50-cloud-init.yaml
 sudo netplan apply
 echo -en "\007"
